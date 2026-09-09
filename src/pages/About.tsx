a560938bc16f7e19f90ad8c7455c75f8e2699ad7
ethan-harper81 <114507050+ethan-harper81@users.noreply.github.com>
2026-09-09 00:51:05 -0400

    Fix about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,17 @@
 import AboutHeader from "../components/AboutHeader";
+import NavBar from "../components/NavBar";
+import HorizontalBar from "../components/HorizontalBar";
 import TopCornerMenu from "../components/TopLeftMenu";
 
 export default function About() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-orange-900 border-8 border-amber-50 text-amber-50 rounded-md">
+    <div className="relative
+  min-h-screen
+  bg-orange-900
+  border-8 border-amber-50
+  text-amber-50
+  rounded-md
+  px-6 py-16">
         <TopCornerMenu items={[
             { message: "Home", link: "/" },
             
